114c6bde2c8f3ef05e252c97e5fde036c2b0aeb6
udPhish <28509694+udPhish@users.noreply.github.com>
2021-08-19 10:04:30 -0400

    Debugging

--- udphGit.cmake
+++ udphGit.cmake
@@ -196,7 +196,7 @@ function(git_update_submodule submodule_dir)
 	endif()
 endfunction()
 macro(git_add_submodule directory remote)
-	message("d ${directory} ${${PROJECT_NAME}_GIT_SUBMODULE_DIRS}")
+	message("d ${directory} , ${${PROJECT_NAME}_GIT_SUBMODULE_DIRS}")
 	if(NOT ${directory} IN_LIST ${PROJECT_NAME}_GIT_SUBMODULE_DIRS)
 		execute_process(COMMAND ${GIT_EXECUTABLE} submodule add --force -- "${remote}" "${directory}"
 						WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
